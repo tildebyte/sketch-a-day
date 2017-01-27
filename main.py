6d5cd64fd29b5fd825f2aa394c5687c2b7b96609
Ben Alkov <balkov1@lenovo.com>
2017-01-27 09:42:02 -0500

    Styled page.

--- main.py
+++ main.py
@@ -8,6 +8,68 @@ import cloudstorage as gcs
 
 from google.appengine.api import app_identity as ai
 
+top = '''
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300|Source+Serif+Pro">
+        <style>
+            * {
+                box-sizing: border-box;
+                text-align: center;
+                border-radius: 5px;
+                margin: auto;
+                padding: 5px;
+                color: hsl(36, 100%, 98%);
+            }
+
+            body {
+                font-family: 'Fira Sans', sans-serif;
+                font-family: 'Source Serif Pro', serif;
+                background-color: hsl(38, 42%, 12%);
+            }
+
+            #title-box {
+                color: hsl(39, 23%, 53%);
+                font-family: sans-serif;
+                font-size: 66px;
+            }
+
+            #text-box {
+                background-color: hsl(39, 66%, 35%);
+                width: 90%;
+                font-size: 32px;
+            }
+
+            p {
+                width: 80%;
+                font-family: serif;
+            }
+
+            #highlight {
+                background-color: hsl(39, 23%, 53%);
+            }
+
+            #highlight-date {
+                color: hsl(39, 23%, 68%);
+            }
+        </style>
+    </head>
+
+    <body>
+        <div id="title-box">Sketch-a-Day</div>
+        <div id="text-box">
+'''
+
+bottom = '''
+        </div>
+
+    </body>
+
+</html>
+'''
+
 default_retry_parms = gcs.RetryParams(initial_delay=0.2,
                                       max_delay=5.0,
                                       backoff_factor=2,
@@ -39,7 +101,6 @@ class MainPage(webapp2.RequestHandler):
             self.data['current_tool'] = random.choice(self.data['tools'])
             self.data['prompts'][self.data['current_prompt']] = False
             self.write_file(filename, yaml.safe_dump(self.data))
-            # return data['current_prompt'], data['current_tool']
 
     def get(self):
         bucket_name = os.environ.get('BUCKET_NAME',
@@ -48,8 +109,11 @@ class MainPage(webapp2.RequestHandler):
         filename = bucket + '/data.yaml'
         self.today = dt.date.today()
         self.do_it(filename)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Prompt for {0}: {1}, using {2}.'.format(
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(top)
+        self.response.write('<p>Prompt for <span id="highlight-date">{0}</span>: <span id="highlight">{1}</span>, using <span id="highlight">{2}</span>.</p>'.format(
             self.today, self.data['current_prompt'], self.data['current_tool']))
+        self.response.write(bottom)
+
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
