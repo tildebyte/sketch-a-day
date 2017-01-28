77828672cc95f99b45e820d4b3272d47da4f50cb
Ben Alkov <ben.alkov@gmail.com>
2017-01-28 12:47:05 -0500

    Factor out assets, make other assets findable
    - Fix styling
    - GH Logo

---
+++ sketchadayhtml.py
@@ -0,0 +1,22 @@
+html_text = '''<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300|Source+Serif+Pro">
+        <link rel="stylesheet" type="text/css" href="static/sketch-a-day.css">
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <div id="title-box">Sketch-a-Day</div>
+            <div id="text-box">
+                <p>Prompt for <span id="highlight-date">{0}</span> &mdash; <span id="highlight">{1}</span> using <span id="highlight">{2}</span></p>
+            </div>
+            <div><a id="link" href="https://github.com/tildebyte/sketch-a-day" target="_blank">Source on GitHub</a></div>
+            <a href="https://github.com/tildebyte/sketch-a-day target="_blank""><img id="gh-mark" src="static/GitHub-Mark-Light-32px.png" alt="GitHub logo"></a>
+        </div>
+    </body>
+
+</html>
+'''
