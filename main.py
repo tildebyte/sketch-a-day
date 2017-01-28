4bdf40235acc165b781efb37e632dfce1153bda6
Ben Alkov <ben.alkov@gmail.com>
2017-01-28 12:47:43 -0500

    Oops, this needs to go with the previous commit.

--- main.py
+++ main.py
@@ -6,69 +6,9 @@ import yaml
 import webapp2
 import cloudstorage as gcs
 
+import sketchadayhtml
+
 from google.appengine.api import app_identity as ai
-
-top = '''
-<!DOCTYPE html>
-<html>
-
-    <head>
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300|Source+Serif+Pro">
-        <style>
-            * {
-                box-sizing: border-box;
-                text-align: center;
-                border-radius: 5px;
-                margin: auto;
-                padding: 5px;
-                color: hsl(36, 100%, 98%);
-            }
-
-            body {
-                font-family: 'Fira Sans', sans-serif;
-                font-family: 'Source Serif Pro', serif;
-                background-color: hsl(38, 42%, 12%);
-            }
-
-            #title-box {
-                color: hsl(39, 23%, 53%);
-                font-family: sans-serif;
-                font-size: 66px;
-            }
-
-            #text-box {
-                background-color: hsl(39, 66%, 35%);
-                width: 90%;
-                font-size: 32px;
-            }
-
-            p {
-                width: 80%;
-                font-family: serif;
-            }
-
-            #highlight {
-                background-color: hsl(39, 23%, 53%);
-            }
-
-            #highlight-date {
-                color: hsl(39, 23%, 68%);
-            }
-        </style>
-    </head>
-
-    <body>
-        <div id="title-box">Sketch-a-Day</div>
-        <div id="text-box">
-'''
-
-bottom = '''
-        </div>
-
-    </body>
-
-</html>
-'''
 
 default_retry_parms = gcs.RetryParams(initial_delay=0.2,
                                       max_delay=5.0,
@@ -110,10 +50,6 @@ class MainPage(webapp2.RequestHandler):
         self.today = dt.date.today()
         self.do_it(filename)
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(top)
-        self.response.write('<p>Prompt for <span id="highlight-date">{0}</span>: <span id="highlight">{1}</span>, using <span id="highlight">{2}</span>.</p>'.format(
-            self.today, self.data['current_prompt'], self.data['current_tool']))
-        self.response.write(bottom)
-
+        self.response.write(sketchadayhtml.html_text.format(self.today, self.data['current_prompt'], self.data['current_tool']))
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
