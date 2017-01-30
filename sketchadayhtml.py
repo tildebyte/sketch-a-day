2d18aee2af6430bfc19e965d51fac31ee31c4771
Ben Alkov <ben.alkov@gmail.com>
2017-01-29 22:33:23 -0500

    Fix styling on today's prompt

--- sketchadayhtml.py
+++ sketchadayhtml.py
@@ -10,8 +10,8 @@ htmltext = '''<!DOCTYPE html>
         <div id="wrapper">
             <div id="title-box">Sketch-a-Day</div>
             <div id="text-box">
-                <p>Prompt for <span id="highlight-date">{0}</span></p>
-                <p><span id="highlight">{1}</span> using <span id="highlight">{2}</span></p>
+                <p class="prompt">Prompt for <span id="highlight-date">{0}</span></p>
+                <p class="prompt"><span id="highlight">{1}</span> using <span id="highlight">{2}</span></p>
             </div>
             <div><a id="link" href="https://github.com/tildebyte/sketch-a-day" target="_blank">Source on GitHub</a></div>
             <a href="https://github.com/tildebyte/sketch-a-day target="_blank""><img id="gh-mark" src="GitHub-Mark-32px.png" alt="GitHub logo"></a>
