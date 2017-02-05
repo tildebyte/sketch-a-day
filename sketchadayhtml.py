046a3c77bc2f81f7d8af96b2a0f2db1df4d0541e
Ben Alkov <ben.alkov@gmail.com>
2017-02-04 22:52:58 -0500

    Fix gallery again
    Tweak layout/styles

--- sketchadayhtml.py
+++ sketchadayhtml.py
@@ -12,14 +12,14 @@ htmltext = '''<!DOCTYPE html>
                 <p>Prompt for <span class="highlight-date">{0}</span></p>
                 <p><span>{1}</span> using <span>{2}</span></p>
             </div>
-            <div><a href="https://github.com/tildebyte/sketch-a-day" target="_blank">Source on GitHub</a></div>
-            <div id="gh-icon">
-                <a href="https://github.com/tildebyte/sketch-a-day" target="_blank"></a>
-            </div>
         </div>
         <p>Sketch and Drawing tutorials</p>
         <div class="gallery">
             {3}
+        </div>
+        <div><a href="https://github.com/tildebyte/sketch-a-day" target="_blank">Source on GitHub</a></div>
+        <div id="gh-icon">
+            <a href="https://github.com/tildebyte/sketch-a-day" target="_blank"></a>
         </div>
         <p>Previous prompts</p>
         <div class="history">
